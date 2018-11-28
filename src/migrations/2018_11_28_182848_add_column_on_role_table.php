9513221004fb89ca180d1b99f31413cfe11bfdc1
Shidi Lateh <shidilateh@yahoo.com>
2018-11-29 02:47:42 +0800

    update migration add role & permission table column name client id for passport multiple client user

---
+++ src/migrations/2018_11_28_182848_add_column_on_role_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnOnRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->unsignedInteger('client_id')->after('slug');
+
+            $table->foreign('client_id')->references('id')->on('oauth_clients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+            $table->dropColumn('client_id');
+        });
+    }
+}
